fec0024a5edb6fcfe087b2e2134819a866e48465
esameth <esameth1@gmail.com>
2021-03-27 16:49:15 -0400

    setup.py to install

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='kaleido',
+    version='1.0',
+    description='Kaleido Interview - Determining Well Compound',
+    maintainer='Elysha Sameth',
+    maintainer_email='esameth1@gmail.com',
+    url='https://github.com/esameth/kaleido',
+
+    classifiers=['Programming Language :: Python :: 3.7'],
+
+    entry_points='''
+        [console_scripts]
+        kaleido = kaleido.command:main
+
+        [kaleido.commands]
+        compound = kaleido.commands.compound:CompoundCommand
+    ''',
+
+    test_suite='kaleido.tests',
+)
